dd5264086b4a1dcac653d76893bfad337ad7e4a8
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2025-02-17 02:06:13 +0600

    jwt sectet define

--- lib/auth.ts
+++ lib/auth.ts
@@ -22,6 +22,7 @@ export const {
   session: {
     strategy: "jwt",
   },
+  secret: process.env.AUTH_SECRET,
   adapter: MongoDBAdapter(mongoClient),
   // adapter: PrismaAdapter(prisma),
   providers: [
